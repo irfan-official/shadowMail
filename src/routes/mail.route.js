8a125fe67089b50a1321b698ee00ccf1dca4e7ad
irfan-official <irfanofficial.work@gmail.com>
2025-05-05 21:28:14 +0600

    fixxxed typo

--- src/routes/mail.route.js
+++ src/routes/mail.route.js
@@ -1,11 +1,14 @@
 import express from "express";
 import authenticateUser from "../middlewares/protectedRoute.middleware.js";
 import sendMail from "../controllers/sendMail.controller.js";
-import { inboxReceiveMail, inboxSendMail } from "../controllers/inbox.controller.js";
+import {
+  inboxReceiveMail,
+  inboxSendMail,
+} from "../controllers/inbox.controller.js";
 
 const router = express.Router();
 
 router.post("/sendmail", authenticateUser, sendMail);
-router.post("/inbox/receive", authenticateUser, receiveMail);
-router.post("/inbox/send", authenticateUser, receiveMail);
+router.post("/inbox/receive", authenticateUser, inboxReceiveMail);
+router.post("/inbox/send", authenticateUser, inboxSendMail);
 export default router;
